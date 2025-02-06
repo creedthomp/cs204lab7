f5208505308cb34b0b5b0043b21c2550123ed320
creedthomp <ctt12@byu.edu>
2025-02-06 10:32:43 -0700

    code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,69 +1,49 @@
-// attempt 3 again fix
+import java.util.UUID;
+
 class Calculator {
 
-    Calculator(){
-
+    Calculator() {
     }
 
-    int add(int a , int b){
+    int add(int a, int b) {
         return a + b;
     }
 
-    int subtract(int a , int b){
+    int subtract(int a, int b) {
         return a - b;
     }
 
-    int multiply(int a , int b){
+    int multiply(int a, int b) {
         return a * b;
     }
 
-    int divide(int a , int b){
+    int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
         return a / b;
     }
 
-
-    /*
-    Returns the n'th number in the fibonacci sequence
-    https://en.wikipedia.org/wiki/Fibonacci_number
-    Example below
-    n = x
-    1 = 1
-    2 = 1
-    3 = 2
-    4 = 3
-    5 = 5
-    .
-    .
-    .
-    etc
-     */
-    int fibonacciNumberFinder(int n){
-        return 0;
+    int fibonacciNumberFinder(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n must be a positive integer.");
+        } else if (n == 1 || n == 2) {
+            return 1;
+        }
+        int a = 1, b = 1, c = 0;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 
-
-    /*
-    Returns binary value of the given int number
-    The binary number will be stored as a string
-    if int a = 0 then this method returns: 0
-    if int a = 10 then this method returns: 1010
-    if int a = 16 then this method returns: 10000
-     */
-    String intToBinaryNumber(int number){
-        return null;
+    String intToBinaryNumber(int number) {
+        return Integer.toBinaryString(number);
     }
 
-    /*
-    Create a completely unique String identifier for a given string
-    Each createdID must contain the string n in its unaltered Form
-    if String n = "Jason"
-    then the created ID could be = Jasonklfgn3jknnvksdfm - Because it contains the unaltered String n and is unique
-
-    if you run this function twice with the same String input, it must return 2 unique String IDs
-     */
-    String createUniqueID(String n){
-        return null;
+    String createUniqueID(String n) {
+        return n + UUID.randomUUID().toString();
     }
-
-
 }
